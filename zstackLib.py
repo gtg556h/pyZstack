39686d28b79d7dd9344a8b9a3e2e2318e492dddd
brian <brn.j.williams@gmail.com>
2014-03-25 18:56:58 -0500

    updated code, initiated sharpness algorithm, added resources and notes

--- zstackLib.py
+++ zstackLib.py
@@ -1,11 +1,23 @@
+##############################################
+#  Library of tools for z-stack analysis     #
+#  Brian Williams                            #
+#  2014.03.25                                #
+#  brn.j.williams@gmail.com                  #
+##############################################
+
+
 from __future__ import division
+import zstackLib
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import scipy
 import cv2
 from scipy import ndimage
+import scipy.signal
 
+###############################
+###############################
 
 class zstack(object):
     def __init__(self, filename):
@@ -13,6 +25,7 @@ class zstack(object):
         self.countFrames()
 
 
+    ##########################################################
     def countFrames(self):
         i=0
         while 1:
@@ -23,12 +36,13 @@ class zstack(object):
                 break
             i+=1
                 
-
-    def scan(self, threshValue=127, maxValue=255, adaptive=0):
+    ###########################################################
+    def focusScan(self, threshValue=127, maxValue=255, adaptive=0):
         self.totalSize = []
         self.nParticles = []
+        self.sharpness = []
         j = 0
-        while 1: 
+        while j<40: #1: 
             try:
                 self.im.seek(j)
                 print(j)
@@ -69,11 +83,15 @@ class zstack(object):
             self.totalSize.append(np.sum(particleSize[1:particleSize.shape[0]]))
             j+=1
 
-		#self.totalSize = np.asarray(self.totalSize)
-		#self.nParticles = np.asarray(self.nParticles)
+            self.sharpness.append(zstackLib.sharpnessLaplace(gray))
 
-            
-    def scanTrial(self):
+        self.totalSize = np.asarray(self.totalSize)
+        self.nParticles = np.asarray(self.nParticles)
+        self.sharpness = np.asarray(self.sharpness)
+
+
+    #######################################################        
+    def scanTemplate(self):
         i = 0
         while 1:
             try: 
@@ -85,6 +103,43 @@ class zstack(object):
 
             i+=1
 
+    ######################################################
 
+    ######################################################
+    def sharpnessFourier(self):
+        # High frequency content indicates sharpeness
+        print('write some code')
+
+
+
+    ######################################################
     def writeImage(self,filename,im):
         scipy.misc.imsave(filename,im)
+
+
+##########################################
+def sharpnessLaplace(image):
+    # Convolve image w/ laplacian kernel: 
+    #    1
+    # 1 -4  1
+    #    1
+
+    laplaceKernel = np.array([[0,1,0],[1,-4,1],[0,1,0]])
+    image2 = np.zeros([image.shape[0]+4,image.shape[1]+4])
+    image2[2:image.shape[0]+2,2:image.shape[1]+2] = image
+    for i in range(2,image.shape[0]+2):
+        image2[i,0] = image2[i,2]
+        image2[i,1] = image2[i,2]
+        image2[i,image2.shape[1]-2] = image2[i,image.shape[1]-3]
+        image2[i,image2.shape[1]-1] = image2[i,image.shape[1]-3]
+    for i in range(2,image.shape[1]+2):
+        image2[0,i] = image2[2,i]
+        image2[1,i] = image2[2,i]
+        image2[image2.shape[0]-2,i] = image2[image2.shape[0]-3,i]
+        image2[image2.shape[0]-1,i] = image2[image2.shape[0]-3,i]
+
+    conv = np.abs(scipy.signal.convolve2d(image2,laplaceKernel))
+    return np.sum(conv)
+
+
+
