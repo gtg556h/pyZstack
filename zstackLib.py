edb1fed2dbec6ef642960ec1175eedea942135db
brian <brn.j.williams@gmail.com>
2014-04-14 15:32:59 -0500

    updated pad algorithm

--- zstackLib.py
+++ zstackLib.py
@@ -5,6 +5,8 @@
 #  brn.j.williams@gmail.com                  #
 ##############################################
 
+# Finish padImage function
+
 
 from __future__ import division
 import zstackLib
@@ -42,7 +44,7 @@ class zstack(object):
         self.nParticles = []
         self.sharpness = []
         j = 0
-        while j<40: #1: 
+        while j<self.nFrames: #1: 
             try:
                 self.im.seek(j)
                 print(j)
@@ -83,11 +85,15 @@ class zstack(object):
             self.totalSize.append(np.sum(particleSize[1:particleSize.shape[0]]))
             j+=1
 
-            self.sharpness.append(zstackLib.sharpnessLaplace(gray))
+            sharpness, conv = zstackLib.sharpnessLaplace(gray)
+            self.sharpness.append(sharpness)
+            #self.sharpness.append(zstackLib.sharpnessLaplace(gray))
+            
 
         self.totalSize = np.asarray(self.totalSize)
         self.nParticles = np.asarray(self.nParticles)
         self.sharpness = np.asarray(self.sharpness)
+        self.conv = conv
 
 
     #######################################################        
@@ -116,8 +122,20 @@ class zstack(object):
     def writeImage(self,filename,im):
         scipy.misc.imsave(filename,im)
 
+    ######################################################
+
+    def showImage(self,frame):
+        try:
+            self.im.seek(frame)
+            image = np.array(self.im)
+            plt.imshow(image,'gray')
+            plt.show()
+        except EOFError:
+            print('Bad frame number')
+
 
 ##########################################
+
 def sharpnessLaplace(image):
     # Convolve image w/ laplacian kernel: 
     #    1
@@ -125,21 +143,37 @@ def sharpnessLaplace(image):
     #    1
 
     laplaceKernel = np.array([[0,1,0],[1,-4,1],[0,1,0]])
-    image2 = np.zeros([image.shape[0]+4,image.shape[1]+4])
-    image2[2:image.shape[0]+2,2:image.shape[1]+2] = image
-    for i in range(2,image.shape[0]+2):
-        image2[i,0] = image2[i,2]
-        image2[i,1] = image2[i,2]
-        image2[i,image2.shape[1]-2] = image2[i,image.shape[1]-3]
-        image2[i,image2.shape[1]-1] = image2[i,image.shape[1]-3]
-    for i in range(2,image.shape[1]+2):
-        image2[0,i] = image2[2,i]
-        image2[1,i] = image2[2,i]
-        image2[image2.shape[0]-2,i] = image2[image2.shape[0]-3,i]
-        image2[image2.shape[0]-1,i] = image2[image2.shape[0]-3,i]
+
+    # Pad image:
+    image2 = zstackLib.padImage(image,2)
+    print(image2.dtype)
 
     conv = np.abs(scipy.signal.convolve2d(image2,laplaceKernel))
-    return np.sum(conv)
+    #return np.sum(conv),conv
+    return np.percentile(np.ravel(conv),99.999),conv
 
 
+############################################################3
+
+def padImage(image,pad):
+    image2 = np.zeros([image.shape[0]+2*pad, image.shape[1]+2*pad])
+    image2[pad:image.shape[0]+pad, pad:image.shape[1]+pad] = image
+
+    d0 = image2.shape[0]
+    d1 = image2.shape[1]
+
+    for i in range(pad,d0-pad):
+        image2[i,0:pad] = image2[i,pad]
+        image2[i,d1-pad:d1] = image2[i,d1-pad-1]
+    
+    for i in range(pad,d1-pad):
+        image2[0:pad,i] = image2[pad,i]
+        image2[d0-pad:d0,i] = image2[d0-pad-1,i]
+
+    image2[0:pad,0:pad] = image2[pad,pad]
+    image2[0:pad,d1-pad:d1] = image2[pad,d1-pad-1]
+    image2[d0-pad:d0,0:pad] = image2[d0-pad-1,pad]
+    image2[d0-pad:d0,d1-pad:d1] = image2[d0-pad-1,d1-pad-1]
+
+    return image2
 
